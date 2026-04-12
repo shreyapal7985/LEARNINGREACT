e923f1ab587191da40f282a36fd165f2b7e046b1
Shreya pal <shreyapal7985@gmail.com>
2026-04-12 12:51:01 +0530

    practiced about topic pressable btn

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import App2 from './component/PracticeSheet/D_Modal';
+import App2 from './component/PracticeSheet/Pressable';
 
 
 const App = () => {
--- component/PracticeSheet/Pressable.js
+++ component/PracticeSheet/Pressable.js
@@ -1,1 +1,40 @@
-import 
+import React from "react";
+import {View,Text,Pressable,StyleSheet  } from 'react-native';
+const App2=()=>{
+    return(
+        <View style={styles.main}>
+            <Pressable 
+            onLongPress={()=>alert("hello")}
+            delayLongPress={3000}
+           // onPressIn={()=>alert("pressin")}
+            //onPressOut={()=>alert("press out")}
+            >
+                <View style={styles.btn}>
+                <Text style={{fontSize:20}}>Pressable</Text>
+                </View>
+            </Pressable>
+        </View>
+    )
+}
+const styles=StyleSheet.create(
+    {
+        main:{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center'
+        },
+        
+        btn:{
+            padding:20,
+            backgroundColor:"white",
+            borderRadius:20,
+            width:200,
+            alignItems:'center'
+            
+            
+        
+        }
+    }
+)
+
+export default App2;
