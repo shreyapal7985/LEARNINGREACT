b056fac632209435e047180f71c485adf5612c5b
Shreya pal <shreyapal7985@gmail.com>
2026-04-13 22:48:26 +0530

    practiced about styling the stack navigation and applying btn on it

--- component/PracticeSheet/H_StackNavigation.js
+++ component/PracticeSheet/H_StackNavigation.js
@@ -1,16 +1,43 @@
-import { View,Text,Button } from "react-native";
+import { View,Text,Button, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack=createNativeStackNavigator();
 const App2=()=>{
     return(
         <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name='Login' component={Login}/>
+        <Stack.Navigator
+        screenOptions={{//from there style can be applied to every navigation
+            headerStyle:{
+            backgroundColor:'grey'},
+            headerTintColor:'pink',
+            headerTitleStyle:{
+                fontSize:40
+            }
+        }}>
+
+            <Stack.Screen name='Login' component={Login} 
+            options={{ //from here styles can be applied to specific element
+            headerLeft:()=><Button title='left'/>,
+            headerRight:()=><Header/>,
+                title:'user',
+                headerStyle:{
+                    backgroundColor:'red'
+                },
+                headerTintColor:'white',
+                headerTitleStyle:{
+                 fontSize:50,
+                }
+
+            }}/>
             <Stack.Screen name='Home' component={Home}/>
         </Stack.Navigator>
 
         </NavigationContainer>
+    )
+}
+const Header=()=>{
+    return(
+        <TextInput placeholder="name"/>
     )
 }
 const Home=()=>{
