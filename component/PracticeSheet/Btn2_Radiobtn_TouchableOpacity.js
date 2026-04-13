8f4698dfc85020003f818e988e8662756f3b72c2
Shreya pal <shreyapal7985@gmail.com>
2026-04-13 23:00:32 +0530

    practiced

---
+++ component/PracticeSheet/Btn2_Radiobtn_TouchableOpacity.js
@@ -0,0 +1,11 @@
+import {View,Text,Button,TouchableOpacity} from 'react-native;'
+const App2=()=>{
+    return(
+        <View>
+            <TouchableOpacity>
+                
+            </TouchableOpacity>
+        </View>
+    )
+}
+export default App2;
