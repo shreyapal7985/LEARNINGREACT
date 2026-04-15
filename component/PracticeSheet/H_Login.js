1e89fafaa8b5143a926fd511f9aef0d56139ade2
Shreya pal <shreyapal7985@gmail.com>
2026-04-15 21:20:46 +0530

    have learned about how to pass dynamic data from one screen to another in stack navigation

--- component/PracticeSheet/H_Login.js
+++ component/PracticeSheet/H_Login.js
@@ -1,11 +1,15 @@
- import {View,Text,Button} from 'react-native';
+ import React,{useState} from 'react';
+ import {View,Text,Button, TextInput} from 'react-native';
   const Login=(props)=>{
-    const name='shreya';
+    const [name,setName]=useState("");
     const age=34
     return(
          
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
             <Text style={{fontSize:20}}>Login Page</Text>
+            <TextInput placeholder='enter your name' placeholderTextColor={'black'} onChangeText={(text)=>setName(text)}
+                style={{color:'black',borderColor:'black',borderWidth:5}}
+            />
             <Button title="go to home " onPress={()=>props.navigation.navigate("Home",{name,age})}></Button>
         </View>
     )
