2cc7be8f2cc643a6c722d40a48fc1adb14e4d64a
Shreya pal <shreyapal7985@gmail.com>
2026-05-05 21:12:24 +0530

    have made update ui

--- component/PracticeSheet/I5_Crud.js
+++ component/PracticeSheet/I5_Crud.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { View,Text,Button, StyleSheet } from "react-native";
+import { View,Text,Button, StyleSheet, Modal } from "react-native";
 const App2=()=>{
     const [data,setData]=useState('');
+    const [showModal,setShowModal]=useState(false);
+    const [selectedUser,setSelectedUser]=useState(undefined);
     const getapi=async()=>{
         const url='http://192.168.1.8:3000/users';
         let result=await fetch(url);
@@ -24,6 +26,11 @@ const App2=()=>{
     useEffect(()=>{
         getapi();
     },[])
+
+const updateData=(data)=>{
+    setShowModal(true)
+    setSelectedUser(data)
+}
     return(
         <View style={styles.container}>
         <View style={styles.innerContainer}>
@@ -38,11 +45,21 @@ const App2=()=>{
             <View style={styles.innerView}><Text>{item.name}</Text></View>
             <View style={styles.innerView}><Text>{item.age}</Text></View>
             <View style={styles.innerView}><Button title="delete" color={'red'} onPress={()=>datadlt(item.id)}/></View>
-            <View style={styles.innerView}><Button title="update" color={'grey'}/></View></View>)
+            <View style={styles.innerView}><Button title="update" color={'grey'} onPress={()=>updateData(item)} /></View></View>)
             :null}
+            <Modal visible={showModal} transparent={true}>
+<Users setShowModal={setShowModal} selectedUser={selectedUser}/>
+            </Modal>
         </View>
     )
 }
+const Users=(props)=>{
+    return(
+<View style={styles.modalCenter}>
+    <View style={styles.modalText}><Text style={{fontSize:30}}>hello{props.selectedUser.name}</Text>
+    <Button title="close" color={'grey'} onPress={()=> props.setShowModal(false)} /></View>
+</View>
+)}
 const styles=StyleSheet.create({
     container:{
         flex:1
@@ -57,6 +74,21 @@ const styles=StyleSheet.create({
     },
     innerView:{
         flex:1
+    },
+    modalCenter:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        
+    },
+    modalText:{
+        borderColor:'white',
+        borderWidth:3,
+        padding:30,
+        backgroundColor:'pink',
+        borderRadius:25,
+        width:300
     }
+
 })
 export default App2;
